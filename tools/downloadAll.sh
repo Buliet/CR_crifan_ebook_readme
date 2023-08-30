1217df6e5d5c5d99d3d49a7e15dfab8f5cecf0e8
Buliet <czhgoi@gmail.com>
2023-08-30 16:43:03 +0800

    optimize download

--- tools/downloadAll.sh
+++ tools/downloadAll.sh
@@ -1,2 +1,50 @@
-wget -r -np -nH --cut-dirs=1 -e robots=off -P source/ https://www.crifan.com/files/doc/docbook/
-wget -r -np -nH --cut-dirs=1 -e robots=off -P source/ https://book.crifan.com/books/
+wget -c -nc -r -np -nH --cut-dirs=1 -e robots=off -P source/docbook https://www.crifan.com/files/doc/docbook/ > docbook.log 2>&1 &
+wget -c -nc -r -np -nH --cut-dirs=1 -e robots=off -P source/gitbook https://book.crifan.com/books/ > gitbook.log 2>&1 &
+
+wait
+echo "------抓取完成------"
+
+find source -name 'index.html' -print|grep -v website|grep -v htmls|grep -v webhelp|xargs rm -f
+
+cd source/
+
+cd gitbook/
+echo "------$(pwd)------"
+for file in $(ls -d *)
+do
+    if [ ! -d $file ]
+    then
+        continue
+    fi
+    if [ ! -f $file/pdf/$file.pdf ]
+    then
+        echo "[$file] pdf missing !"
+    fi
+    if [ ! -f $file/website/index.html ]
+    then
+        echo "[$file] html missing !"
+    fi
+done
+cd ..
+
+cd docbook/doc/docbook/
+echo "------$(pwd)------"
+for file in $(ls -d *)
+do
+    if [ ! -d $file ]
+    then
+        continue
+    fi
+    if [ ! -f $file/release/pdf/$file.pdf ]
+    then
+        echo "[$file] pdf missing !"
+    fi
+    if [ ! -f $file/release/html/$file.html ]
+    then
+        echo "[$file] html missing !"
+    fi
+    if [ ! -f $file/release/htmls/index.html ]
+    then
+        echo "[$file] htmls missing !"
+    fi
+done
